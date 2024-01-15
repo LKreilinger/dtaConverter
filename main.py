a53895e9126c9dd0a6ad888efbd5f661cb28e659
LKreilinger <laurens.kreilinger@web.de>
2024-01-15 10:56:02 +0100

    replace "," with "."

--- main.py
+++ main.py
@@ -2,20 +2,21 @@ import os
 from pathlib import Path
 
 if __name__ == '__main__':
-
+    # Change here your path
     mypath: str = r"H:\01_Projekte\Elektroden_Charakterisierung\EIS_Messungen"
-    #mypath: str = r"H:\01_Projekte\Elektroden_Charakterisierung\Python\dtaConverter"
+    #
+
     for file in os.listdir(mypath):
         if file.endswith(".DTA"):
             fileExExtension = Path(file).stem
             FilePath = os.path.join(mypath, fileExExtension)
 
             fOrig = open(FilePath + ".DTA", "r")
-            fNew = open(FilePath + ".txt", "a")
+            fNew = open(FilePath + ".txt", "w")
             lFound = 0
             for line in fOrig:
                 if '	#	s	Hz	ohm	' in line or lFound == 1:
-                    #print(line)
+                    line = line.replace(",", ".")
                     fNew.write(line)
                     lFound = 1
 
